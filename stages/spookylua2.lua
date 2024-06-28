bc27ba05c93f06f1d2b7a9aa647a7098b9660c3e
Raphixxzy <pao13662@gmail.com>
2024-06-27 23:39:02 -0300

    Delete stages directory

--- stages/spookylua2.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-makeLuaSprite('halloweenBG', 'stages/wek2/back', 200, 0)
-scaleObject('halloweenBG', 2, 2)
-makeLuaSprite('halloweenBG2', 'stages/wek2/overlay', 200, 0)
-scaleObject('halloweenBG2', 2, 2)
-
-	addLuaSprite('halloweenBG', false);
-	addLuaSprite('halloweenBG2', true);
-end
