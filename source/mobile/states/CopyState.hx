47b121128e8156e4db22ee8dd59f4999ba942674
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-04-20 16:53:30 +0800

    Update CopyState.hx

--- source/mobile/states/CopyState.hx
+++ source/mobile/states/CopyState.hx
@@ -254,7 +254,7 @@ class CopyState extends MusicBeatState
 	public static function getOriginalMD5(file:String)
 	{
     		var bytes = OpenflAssets.getBytes(file);
-		var origMd5 = haxe.crypto.Md5.make(bytes)
+		var origMd5 = haxe.crypto.Md5.make(bytes);
    		return origMd5.toHex;
 	}
 
