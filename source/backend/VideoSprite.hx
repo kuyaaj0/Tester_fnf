a8c410ce8fcdec48f12a639a63701a2a5c0b4a65
beihu235 <2120024137@qq.com>
2025-05-01 18:57:48 +0800

    代码修复

--- source/backend/VideoSprite.hx
+++ source/backend/VideoSprite.hx
@@ -6,7 +6,7 @@ import flixel.addons.display.FlxPieDial;
 import hxvlc.flixel.FlxVideoSprite;
 #end
 
-#if (VIDEOS_ALLOWED && !ios) 
+#if (VIDEOS_ALLOWED_Old) 
 	#if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as VideoHandler;
 	#elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as VideoHandler;
 	#else import VideoHandler;
