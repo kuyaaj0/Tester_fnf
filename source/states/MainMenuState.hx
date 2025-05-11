2f5d458096c6e6111aba96fbfef4ff0ef26cd0ae
狐月影 <105789304+beihu235@users.noreply.github.com>
2025-05-11 16:29:33 +0800

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -25,8 +25,8 @@ import sys.thread.Mutex;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.7.3'; //This is also used for Discord RPC
-	public static var novaFlareEngineDataVersion:Float = 2.5;
-	public static var novaFlareEngineVersion:String = '1.1.8';
+	public static var novaFlareEngineDataVersion:Float = 2.6;
+	public static var novaFlareEngineVersion:String = '1.1.8-HOTFIX';
 
 	public static var NovaFlareGithubAction:String = '????';
 	public static var createTime:String = 'Time: ????';
