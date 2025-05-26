9d27f8d6c0e589b12b4d4ebd88003efa7b64c69e
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-05-27 00:15:32 +0800

    Update Song.hx

--- source/backend/Song.hx
+++ source/backend/Song.hx
@@ -160,7 +160,7 @@ class Song
 
     public static function castVersion(songJson:SwagSong):SwagSong {
 	    for (i in 0...songJson.notes.length){
-		for (ii in i.sectionNotes){
+		for (ii in i.sectionNotes.length){
 			var gottaHitNote:Bool = i.mustHitSection;
 			if(gottaHitNote){
 				if(ii[1] >= 4){
