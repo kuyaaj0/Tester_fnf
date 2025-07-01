bac0fa6b67e00791b1d24bc3d73ee0025a3c4644
beihu235 <2120024137@qq.com>
2025-07-01 20:44:10 +0800

    算了不乱搞了

--- source/Main.hx
+++ source/Main.hx
@@ -213,18 +213,9 @@ class Main extends Sprite
 		if (ClientPrefs.data.gcFreeZone)
 			cpp.NativeGc.enterGCFreeZone;
 		
-		// 1. 设置极高的空闲阈值（抑制自动 GC）
-		cpp.NativeGc.setMinimumFreeSpace(1024 * 1024 * 1024); 
-
-		// 2. 预分配足够工作内存（可选）
-		cpp.NativeGc.setMinimumWorkingMemory(256 * 1024 * 1024);
-
-
 		// shader coords fix
 		FlxG.signals.gameResized.add(function(w, h)
 		{
-			// if(fpsVar != null)
-			// fpsVar.positionFPS(10, 3, Math.min(Lib.current.stage.stageWidth / FlxG.width, Lib.current.stage.stageHeight / FlxG.height));
 			if (FlxG.cameras != null)
 			{
 				for (cam in FlxG.cameras.list)
