139ce1fba8a99d43cccbcf123e67de2e86d8435e
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-07-08 23:37:35 +0800

    Update PlayListWindow.hx

--- source/objects/state/relaxState/windows/PlayListWindow.hx
+++ source/objects/state/relaxState/windows/PlayListWindow.hx
@@ -2,9 +2,9 @@ package objects.state.relaxState.windows;
 
 import flixel.group.FlxSpriteGroup;
 
-class PlayListWindow extends FlxSpriteGroup;
+class PlayListWindow extends FlxSpriteGroup
 {
-    public Hidding:Bool = true;
+    public var Hidding:Bool = true;
 
     public function new()
     {
