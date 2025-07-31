d8a269ec4f2fa67adad24d68c294f9f10edda6e9
beihu235 <2120024137@qq.com>
2025-07-31 13:40:28 +0800

    copystate修复

--- source/mobile/states/CopyState.hx
+++ source/mobile/states/CopyState.hx
@@ -227,7 +227,7 @@ class CopyState extends MusicBeatState
 	{
 		locatedFiles = OpenflAssets.list();
 		// removes unwanted assets
-		var assets = locatedFiles.filter(folder -> folder.startsWith('assets/') && !folder.contains('asset/shared/images/menuExtendHide/'));
+		var assets = locatedFiles.filter(folder -> folder.startsWith('assets/') && !folder.contains('assets/shared/images/menuExtendHide/'));
 		var mods = locatedFiles.filter(folder -> folder.startsWith('mods/'));
 		locatedFiles = assets.concat(mods);
 
