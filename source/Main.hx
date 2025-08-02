7366a8aed86822367c140ab58377dd062f30cb2e
MaoPou <chenghongweng3@outlook.com>
2025-08-02 23:10:19 +0800

    牛逼，初始化放错位置了

--- source/Main.hx
+++ source/Main.hx
@@ -65,10 +65,6 @@ class Main extends Sprite
 
 	public static function main():Void
 	{
-	    if (ClientPrefs.data.developerMode){
-	        TraceInterceptor.init();
-	    }
-	    
 		#if (cpp && windows)
 		backend.device.Native.fixScaling();
 		backend.device.Native.setWindowDarkMode(true, true);
@@ -132,6 +128,10 @@ class Main extends Sprite
 		#if LUA_ALLOWED llua.Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
+		
+		if (ClientPrefs.data.developerMode){
+	        TraceInterceptor.init();
+	    }
 
 		#if mobile
 		#if android
