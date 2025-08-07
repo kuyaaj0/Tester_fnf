9cb0f8fe3c76db71fed26ccc2eef38ea89ba9f45
MaoPou <chenghongweng3@outlook.com>
2025-08-07 11:49:33 +0800

    bbb

--- source/substates/RelaxSubState.hx
+++ source/substates/RelaxSubState.hx
@@ -67,7 +67,7 @@ class RelaxSubState extends MusicBeatSubstate
 	var triangleEmitter:HollowTriangleEmitter;
 	
 	var backendPicture:FlxSprite;
-	public var audio:Dynamic;
+	public var audio:FlxSpriteGroup;
 	var recordPicture:FlxSprite;
 	
 	var oldBackendPicture:FlxSprite;
