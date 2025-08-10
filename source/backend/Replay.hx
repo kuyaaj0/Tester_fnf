f8686e87f1eca99fdd6b1850271be83d1e33454e
kittycathy233 <2286431462@qq.com>
2025-08-10 14:49:10 +0800

    好了Replay已经用deeksuck改完了（然而还是测试不了qwq

--- source/backend/Replay.hx
+++ source/backend/Replay.hx
@@ -5,7 +5,8 @@ import flixel.FlxBasic;
 class Replay extends FlxBasic
 {
 	// 整个组>摁压类型>行数>时间
-	public var hitData:Array<Array<Array<Float>>> = [[[], [], [], []], [[], [], [], []]];
+	public var hitData:Array<Array<Array<Float>>> = [[], []];
+	public var mania:Int = 3; // 添加键位数变量
 
 	var follow:Dynamic; //跟随的state
 
@@ -15,20 +16,26 @@ class Replay extends FlxBasic
 	{
 		super();
 		this.follow = follow;
+		// 初始化键位数
+		if (follow.SONG != null) {
+			mania = follow.SONG.mania;
+		}
+		// 初始化数据结构
+		reset();
 	}
 
 	public function push(time:Float, type:Int, state:Int)
 	{
-		if (!follow.replayMode)
+		if (!follow.replayMode && type < mania)
 			hitData[state][type].push(time);
 	}
 
 	var isPaused:Bool = false;
-	var pauseArray:Array<Float> = [-9999, -9999, -9999, -9999];
+	var pauseArray:Array<Float> = [];
 
 	public function pauseCheck(time:Float, type:Int)
 	{
-		if (follow.replayMode)
+		if (follow.replayMode || type >= mania)
 			return;
 		pauseArray[type] = time;
 		isPaused = true;
@@ -40,34 +47,33 @@ class Replay extends FlxBasic
 		{
 			if (isPaused)
 			{
-				for (key in 0...4)
-					if (!follow.controls.pressed(follow.keysArray[key]) && pauseArray[key] != -9999)
+				for (key in 0...mania)
+					if (key < pauseArray.length && !follow.controls.pressed(follow.keysArray[key]) && pauseArray[key] != -9999)
 						push(pauseArray[key], key, 1);
 
-				pauseArray = [-9999, -9999, -9999, -9999];
+				// 重置暂停数组
+				for (i in 0...mania)
+					pauseArray[i] = -9999;
 				isPaused = false;
 			}
 		}
 		else
 		{
-			for (type in 0...mania) // 使用mania作为循环上限
+			for (type in 0...mania)
 			{
-				if (hitData[1][type].length > 0 && hitData[1][type][0] <= Conductor.songPosition)
+				if (type < hitData[1].length && hitData[1][type].length > 0 && hitData[1][type][0] <= Conductor.songPosition)
 					holdCheck(type);
 			}
 		}
 	}
 
-	var allowHit:Array<Bool> = [true, true, true, true];
+	var allowHit:Array<Bool> = [];
 
 	function holdCheck(type:Int)
 	{
 		if (type >= hitData[0].length || type >= hitData[1].length)
-			return; // 边界检查
-
-		if (hitData[0][type].length == 0 || hitData[1][type].length == 0)
 			return;
-
+			
 		if (hitData[0][type][0] >= Conductor.songPosition)
 		{
 			follow.keysCheck(type, Conductor.songPosition);
@@ -93,26 +99,45 @@ class Replay extends FlxBasic
 
 	public function init()
 	{
-		// 使用mania初始化数组
-		hitData = [[for (i in 0...mania) []], [for (i in 0...mania) []]];
-		for (state in 0...2)
-			for (type in 0...4)
-				for (hit in 0...hitData[state][type].length)
-				{
+		// 只能这么复制 --狐月影
+		hitData = [[], []];
+		for (state in 0...2) {
+			hitData[state] = [];
+			for (type in 0...mania) {
+				hitData[state][type] = [];
+				for (hit in 0...hitData[state][type].length) {
 					hitData[state][type].push(hitData[state][type][hit]);
 				}
 			}
-
-		// 使用mania初始化allowHit
-		allowHit = [for (i in 0...mania) true];
-
-		// 只能这么复制 --狐月影
+		}
+		
+		// 初始化允许命中数组
+		allowHit = [];
+		for (i in 0...mania)
+			allowHit.push(true);
 	}
 
 	public function reset()
 	{
-		hitData = [[[], [], [], []], [[], [], [], []]];
-		pauseArray = [-9999, -9999, -9999, -9999];
+		// 根据键位数动态创建数据结构
+		hitData = [[], []];
+		for (state in 0...2) {
+			hitData[state] = [];
+			for (type in 0...mania) {
+				hitData[state][type] = [];
+			}
+		}
+		
+		// 初始化暂停数组
+		pauseArray = [];
+		for (i in 0...mania)
+			pauseArray.push(-9999);
+		
+		// 初始化允许命中数组
+		allowHit = [];
+		for (i in 0...mania)
+			allowHit.push(true);
+			
 		isPaused = false;
 	}
 
