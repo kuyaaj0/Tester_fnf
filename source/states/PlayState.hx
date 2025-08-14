fd052019b604847ba9fa99279a5db1b5febafc93
beihu235 <2120024137@qq.com>
2025-08-14 22:59:35 +0800

    上传haxeui库

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2698,6 +2698,7 @@ class PlayState extends MusicBeatState
 
 		for (key in 0...keysArray.length)
 		{
+			trace(key);
 			if (controls.pressed(keysArray[key]))
 				replay.pauseCheck(Conductor.songPosition, key);
 			// 暂停时候回放数据的保存，防止出现错误;
