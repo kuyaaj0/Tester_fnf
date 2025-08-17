001803c2cadacf79e1ccecb2b7731437528d5cb7
beihu235 <2120024137@qq.com>
2025-08-17 22:28:31 +0800

    后面方便测试

--- source/Main.hx
+++ source/Main.hx
@@ -50,7 +50,7 @@ class Main extends Sprite
 	private static var game = {
 		width: 1280, // WINDOW width
 		height: 720, // WINDOW height
-		initialState: PassState, // initial game state
+		initialState: #if mobile PassState #else InitState #end, // initial game state
 		zoom: -1.0, // game state bounds
 		framerate: 60, // default framerate
 		skipSplash: true, // if the default flixel splash screen should be skipped
